87ac09596abb5f37e64048a7e0447e7bdf012f5f
bbowsza <bbowsza@gmail.com>
2026-07-21 12:15:38 -0400

    Update sources.py

--- sources.py
+++ sources.py
@@ -1,10 +1,37 @@
 SOURCES = [
-    "BedJet",
-    "Best Buy",
-    "Amazon",
-    "GMA Deals",
-    "MorningSave",
-    "View Your Deal",
-    "Slickdeals",
-    "Reddit"
+    {
+        "name": "BedJet",
+        "type": "shopify",
+        "url": "https://bedjet.com/products/bedjet-3-climate-comfort-system-with-biorhythm-sleep-technology.js"
+    },
+
+    {
+        "name": "Best Buy",
+        "type": "search",
+        "url": "https://www.bestbuy.com/site/searchpage.jsp?st=bedjet+3"
+    },
+
+    {
+        "name": "Amazon",
+        "type": "search",
+        "url": "https://www.amazon.com/s?k=bedjet+3+climate+comfort+system"
+    },
+
+    {
+        "name": "MorningSave",
+        "type": "search",
+        "url": "https://morningsave.com/search?q=bedjet"
+    },
+
+    {
+        "name": "GMA Deals",
+        "type": "search",
+        "url": "https://gmadeals.com/search?q=bedjet"
+    },
+
+    {
+        "name": "Slickdeals",
+        "type": "search",
+        "url": "https://slickdeals.net/newsearch.php?src=SearchBox&q=bedjet+3"
+    }
 ]
