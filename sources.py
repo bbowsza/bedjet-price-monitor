32af73cc82a8a0f8b8e1e14ac9f568bf6e057ca2
bbowsza <bbowsza@gmail.com>
2026-07-24 11:36:14 -0400

    Update sources.py

--- sources.py
+++ sources.py
@@ -1,50 +1,14 @@
 SOURCES = [
-
     {
-        "name": "BedJet",
-        "type": "shopify",
-        "url_template": "https://bedjet.com/products/bedjet-3-climate-comfort-system-with-biorhythm-sleep-technology.js"
+        "name": "Amazon"
     },
-
     {
-        "name": "Best Buy",
-        "type": "search",
-        "url_template": "https://www.bestbuy.com/site/searchpage.jsp?st={query}"
+        "name": "Best Buy"
     },
-
     {
-        "name": "Amazon",
-        "type": "search",
-        "url_template": "https://www.amazon.com/s?k={query}"
+        "name": "Target"
     },
-
     {
-        "name": "Target",
-        "type": "search",
-        "url_template": "https://www.target.com/s?searchTerm={query}"
-    },
-
-    {
-        "name": "Walmart",
-        "type": "search",
-        "url_template": "https://www.walmart.com/search?q={query}"
-    },
-
-    {
-        "name": "MorningSave",
-        "type": "search",
-        "url_template": "https://morningsave.com/search?q={query}"
-    },
-
-    {
-        "name": "GMA Deals",
-        "type": "search",
-        "url_template": "https://gmadeals.com/search?q={query}"
-    },
-
-    {
-        "name": "Slickdeals",
-        "type": "search",
-        "url_template": "https://slickdeals.net/newsearch.php?src=SearchBox&q={query}"
+        "name": "Walmart"
     }
 ]
