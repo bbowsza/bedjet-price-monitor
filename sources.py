ba163945fee37562ecc678e27dcdaf3b7edbf517
bbowsza <bbowsza@gmail.com>
2026-07-24 11:00:40 -0400

    Update sources.py

--- sources.py
+++ sources.py
@@ -2,36 +2,36 @@ SOURCES = [
     {
         "name": "BedJet",
         "type": "shopify",
-        "url": "https://bedjet.com/products/bedjet-3-climate-comfort-system-with-biorhythm-sleep-technology.js"
+        "url_template": "https://bedjet.com/products/bedjet-3-climate-comfort-system-with-biorhythm-sleep-technology.js"
     },
 
     {
         "name": "Best Buy",
         "type": "search",
-        "url": "https://www.bestbuy.com/site/searchpage.jsp?st=bedjet+3"
+        "url_template": "https://www.bestbuy.com/site/searchpage.jsp?st={query}"
     },
 
     {
         "name": "Amazon",
         "type": "search",
-        "url": "https://www.amazon.com/s?k=bedjet+3+climate+comfort+system"
+        "url_template": "https://www.amazon.com/s?k={query}"
     },
 
     {
         "name": "MorningSave",
         "type": "search",
-        "url": "https://morningsave.com/search?q=bedjet"
+        "url_template": "https://morningsave.com/search?q={query}"
     },
 
     {
         "name": "GMA Deals",
         "type": "search",
-        "url": "https://gmadeals.com/search?q=bedjet"
+        "url_template": "https://gmadeals.com/search?q={query}"
     },
 
     {
         "name": "Slickdeals",
         "type": "search",
-        "url": "https://slickdeals.net/newsearch.php?src=SearchBox&q=bedjet+3"
+        "url_template": "https://slickdeals.net/newsearch.php?src=SearchBox&q={query}"
     }
 ]
